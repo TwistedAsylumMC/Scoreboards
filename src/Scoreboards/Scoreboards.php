cecbd531ca414cf7bf0b0f378d0a4a35bf5fe553
PotatoeTrainYT <officialseb.12@gmail.com>
2018-10-20 11:54:08 +0100

    Version 1.0.1

--- src/Scoreboards/Scoreboards.php
+++ src/Scoreboards/Scoreboards.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace Scoreboards;
 
+use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\network\mcpe\protocol\RemoveObjectivePacket;
 use pocketmine\network\mcpe\protocol\SetDisplayObjectivePacket;
 use pocketmine\network\mcpe\protocol\SetScorePacket;
@@ -49,7 +50,14 @@ class Scoreboards extends PluginBase{
 	}
 
 	public function setLine(Player $player, int $score, string $message): void{
-		if(!isset($this->scoreboards[$player->getName()])) return;
+		if(!isset($this->scoreboards[$player->getName()])){
+			$this->getLogger()->error("Cannot set a score to a player with no scoreboard");
+			return;
+		}
+		if($score > 15 || $score < 1){
+			$this->getLogger()->error("Score must be between the value of 1-15. $score out of range");
+			return;
+		}
 		$objectiveName = $this->getObjectiveName($player);
 		$entry = new ScorePacketEntry();
 		$entry->objectiveName = $objectiveName;
@@ -65,5 +73,9 @@ class Scoreboards extends PluginBase{
 
 	public function getObjectiveName(Player $player): ?string{
 		return isset($this->scoreboards[$player->getName()]) ? $this->scoreboards[$player->getName()] : null;
+	}
+
+	public function onQuit(PlayerQuitEvent $event): void{
+		if(isset($this->scoreboards[($player = $event->getPlayer()->getName())])) unset($this->scoreboards[$player]);
 	}
 }
